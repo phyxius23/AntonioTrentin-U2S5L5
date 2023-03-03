b15e3d17a1aed2b1dc3c3fbe255581ae071609f4
phyxius23 <30ntin@gmail.com>
2023-03-03 21:41:35 +0100

    corretto il nome dello scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 /* ADD CLASS ON SCROLL
 -------------------------------------------------------------------------------------------------- */
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function(e) {
    let heightHero = document.querySelector('.hero').offsetHeight;
 
    if(window.scrollY > (heightHero - 150)){
